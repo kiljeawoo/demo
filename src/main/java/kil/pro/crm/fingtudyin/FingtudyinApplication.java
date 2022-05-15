14a2fa491f27e655b7babaf4f4d872481a1e79dd
길재우 <6197@.>
2022-05-16 00:42:39 +0900

    Initial commit

---
+++ src/main/java/kil/pro/crm/fingtudyin/FingtudyinApplication.java
@@ -0,0 +1,13 @@
+package kil.pro.crm.fingtudyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingtudyinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingtudyinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kil/pro/crm/fingtudyin/FingtudyinApplicationTests.java
@@ -0,0 +1,13 @@
+package kil.pro.crm.fingtudyin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingtudyinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
